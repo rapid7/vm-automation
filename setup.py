216e159e74eec270753fb365722435806f39ac1b
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2017-08-17 15:54:30 -0500

    set version 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vm_automation',
-      version='0.1',
+      version='0.1.0',
       description='Virtual infrastructure automation simplified interaction library',
       url='http://github.com/rapid7/vm-automation',
       author='Metasploit Hackers',
